a6f1a4ba8349d251b00c989001f7bce31f3a7ae9
Alex V <vtorov.alex@gmail.com>
2025-04-14 17:22:18 +0300

    Update 3par_class_descr.py

--- 3par_class_descr.py
+++ 3par_class_descr.py
@@ -98,17 +98,17 @@ def main():
 
     hp_logger.info("********************************* Class Discovery is ended *********************************")
     
-    class_description = get_class_description(wbem_conn, "TPD_FCPortSCSIEndpointImplementation")
-    instances = get_instances_of_class(wbem_conn, "TPD_FCPortSCSIEndpointImplementation")
+    class_description = get_class_description(wbem_conn, "TPD_FCPort")
+    instances = get_instances_of_class(wbem_conn, "TPD_FCPort")
     
     # Выводим свойства класса
-    print(f"Properties of TPD_FCPortSCSIEndpointImplementation:")
+    print(f"Properties of TPD_FCPort:")
     for prop_name, prop in class_description.properties.items():
         print(f"{prop_name}: {prop.type} ({prop.qualifiers})")
 
     # Выводим данные экземпляров
     for instance in instances:
-        print(f"Instance of TPD_FCPortSCSIEndpointImplementation:")
+        print(f"Instance of TPD_FCPort:")
         for key, value in instance.items():
             print(f"  {key}: {value}")    
 
